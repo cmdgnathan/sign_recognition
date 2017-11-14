0fce523e44a85406bd22db4c65cecd0e37a3d8d3
albert.cwkuo <albert.cwkuo@gmail.com>
2017-11-13 21:26:43 -0500

    fix negative bbox index bug

--- scripts/overlayTemplate.py
+++ scripts/overlayTemplate.py
@@ -14,10 +14,21 @@ def overlayTemplate(foreground, background):
     background_padded = cv2.copyMakeBorder(background, pad_top, pad_bottom, pad_left, pad_right, cv2.BORDER_CONSTANT, value=[0,0,0])
     # generate random position
     x=np.random.randint(0, wb-1)
-    y=np.random.randint(0, hb-1)
+    #y=np.random.randint(0, hb-1)
+    y=np.clip(hb/2+hb/4*np.random.randn(), a_min=0, a_max=hb-1)
     foreground_rand_pos = np.zeros_like(background_padded)
     foreground_rand_pos[y:y+hf,x:x+wf,:]=foreground
-    bbox = np.array([x-pad_left, y-pad_top, wf, hf])
+    xmin=x-pad_left
+    width=wf
+    ymin=y-pad_top
+    height=hf
+    if xmin<0:
+        width=width+xmin
+        xmin=0
+    if ymin<0:
+        height=height+ymin
+        ymin=0
+    bbox = np.array([xmin, ymin, width, height])
     # generate image mask: 1->foreground, 0->background
     mask = np.any(foreground_rand_pos!=0, axis=2).astype(np.float)
     # blur image mask
